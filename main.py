4d9cf4b4363ade125115e6226775d80d63ecbc6e
knaik <kn172@rutgers.edu>
2023-04-20 06:32:50 -0400

    timeout

--- main.py
+++ main.py
@@ -17,18 +17,18 @@ def scraper(urlChars, stop_event, urlSize):
             url = 'https://' + 'i.imgur.com' + '/' + img + '.jpg'  
             response = requests.head(url)            
             if response.status_code == 200: #assuming 404 not possible, avoiding redirects
-                r = requests.get(url, stream=True, allow_redirects=False)
+                r = requests.get(url, stream=True, allow_redirects=False, timeout=20)
                 if not (r.status_code == 302): #print('Valid[+]:'+img)
                     with open(os.path.join(dir, img)+".jpg", 'wb') as f:
                         for chunk in r.iter_content(4096):
                             f.write(chunk)
                         size = f.tell()//1024
                         if (size//1024>=1):
-                            print("written ",img,str(size//1024)+"mb")
+                            print("written ",img.rjust(7),(str(size//1024)+"mb").rjust(7))
                         elif (f.tell() <= 1024):
-                            print("written ",img,str(f.tell())+"b")
+                            print("written ",img.rjust(7),(str(f.tell())+".b").rjust(7))
                         else:
-                            print("written ",img,str(size)+"kb") # f.tell())
+                            print("written ",img.rjust(7),(str(size)+"kb").rjust(7)) # f.tell())
             elif (response.status_code == 302):                              
                 otherErr.append(img)
             elif (response.status_code == 404):                              
@@ -37,16 +37,18 @@ def scraper(urlChars, stop_event, urlSize):
                 oth.append(img)
             pass
         except requests.exceptions.HTTPError as err:
+            stop_event.set()
             pass
             
-thr = int(input("how many threads? "))
+#thr = int(input("how many threads? "))
 urlChars = list('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890')
 stop_event = threading.Event()
 threads = []
 
-for i in range(thr):
-    lgth = i%3 + 5 # urlSize
-    t = threading.Thread(target=scraper, args=(urlChars, stop_event,lgth))
+
+for i in [5,5,5,5,6,6,7,7]: #range(thr):
+    # lgth = i%3 + 5 # urlSize
+    t = threading.Thread(target=scraper, args=(urlChars, stop_event,i))
     t.start()
     threads.append(t)
 try:
@@ -56,12 +58,11 @@ except KeyboardInterrupt:
     print("Program terminated by user.")
     stop_event.set()
     if (len(otherErr)):
-        print("302 ", otherErr)
+        print("302: ", len(otherErr))
     if (len(notFound)):
-        print("404 ", notFound)
+        print("404 ", len(notFound))
     if (len(oth)):
-        print("other ", oth)
-
+        print("other ", len(oth))
 
 
 for t in threads:
